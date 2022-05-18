b4d6244dcc4d391118aaa90febc5fe5d01f06259
figuraetymologica <50476854+figuraetymologica@users.noreply.github.com>
2022-05-18 20:42:57 +0200

    first upload

---
+++ sketch.js
@@ -0,0 +1,120 @@
+const MODEL_URL = 'models/';
+let canvas;
+let vid;
+let results;
+let landmarks;
+let btn;
+let div;
+let loaded = false;
+
+let happy = false;
+
+function setup() {
+  div = createDiv('<br>face-api models are loading...');
+
+  canvas = createCanvas(270, 480).parent('myCanvas');
+  // use an async callback to load in the models and run the getResults() function
+  vid = createCapture(VIDEO, async () => {
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
+    await faceapi.loadFaceExpressionModel(MODEL_URL);
+    div.elt.innerHTML = '<br>model loaded!';
+    loaded = true;
+    getResults(); // init once
+  }).parent('myCanvas');
+  vid.size(640, 480);
+  vid.hide();
+}
+
+
+
+async function getResults() {
+  results = await faceapi.detectSingleFace(vid.elt).withFaceExpressions();
+  getResults();
+}
+
+
+function draw() {
+  translate(width/2, height/2);
+  scale(-1, 1);
+  background(170, 153, 255);
+  //imageMode(CENTER);
+  //image(vid, 0, 0);
+  let eyeX;
+  let eyeY;
+
+  if (loaded) {
+
+    // results
+    if (results) {
+
+      // draw bounding box
+      let x = results.detection.box.x;
+      let y = results.detection.box.y;
+      let w = results.detection.box.width;
+      let h = results.detection.box.height;
+      noFill();
+      stroke(255);
+      strokeWeight(2);
+      //rect(x - width/2 -width/2, y - height/2, w, h);
+
+      eyeX = x + w/2;
+      eyeY = y + h/2;
+
+      // expressions
+      let expressions = [];
+      for (var expr in results.expressions) {
+        expressions.push([expr, results.expressions[expr]]);
+      }
+
+      // loop trough expressions except last one (because "asSortedArray")
+      for (let i = 0; i < expressions.length - 1; i++) {
+        let label = expressions[i][0];
+        let confidence = expressions[i][1];
+        
+        if (label == "happy" && confidence > 0.8) {
+          happy = true;
+          //text(label, x / 2, y + h / 2);
+        }else if (confidence > 0.8){
+          happy = false;
+        }
+      }
+    }
+  }
+  face(eyeX, eyeY);
+}
+
+function face(addX, addY){
+  let movement = 80;
+  let moveX = 0;
+  let moveY = 0;
+
+  if(addX && addY){
+    moveX = map(addX, 0, vid.width, -movement, movement);
+    moveY = map(addY, 0, vid.height, -movement, movement);
+  }
+
+  ellipseMode(CENTER);
+  fill(255);
+  noStroke();
+  ellipse(-50 + moveX, -50 + moveY, 15);
+  ellipse(50 + moveX, -50 + moveY, 15);
+  stroke(255);
+  strokeWeight(7);
+  beginShape();
+  curveVertex(-75 + moveX, 50 + moveY);
+  curveVertex(-75 + moveX, 50 + moveY);
+  if(happy){
+    noFill();
+    curveVertex(0 + moveX, 75 + moveY);
+  }
+  curveVertex(75 + moveX, 50 + moveY);
+  curveVertex(75 + moveX, 50 + moveY);
+  endShape();
+  resetMatrix();
+}
+
+function mousePressed(){
+  happy = !happy;
+}
